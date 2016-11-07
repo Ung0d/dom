73cfb0ce5f538ed8a9b360e5bbd030b27e6b62b6
Ung0d <fb132550@uni-greifswald.de>
2016-11-07 16:47:36 +0100

    first working tests... still very unstable

--- Entity.h
+++ Entity.h
@@ -1,11 +1,59 @@
 /*
 * This file is part of the dom-library - an implementation of the Entity-Component-System Pattern.
+* Copyright (C) 2016 Felix Becker - fb132550@uni-greifswald.de
+*
+* This software is provided 'as-is', without any express or
+* implied warranty. In no event will the authors be held
+* liable for any damages arising from the use of this software.
+*
+* Permission is granted to anyone to use this software for any purpose,
+* including commercial applications, and to alter it and redistribute
+* it freely, subject to the following restrictions:
+*
+* 1. The origin of this software must not be misrepresented;
+*    you must not claim that you wrote the original software.
+*    If you use this software in a product, an acknowledgment
+*    in the product documentation would be appreciated but
+*    is not required.
+*
+* 2. Altered source versions must be plainly marked as such,
+*    and must not be misrepresented as being the original software.
+*
+* 3. This notice may not be removed or altered from any
+*    source distribution.
 */
 
+#ifndef DOM_ENTITY_H
+#define DOM_ENTITY_H
+
 #include <bitset>
+#include <array>
+#include <unordered_map>
+
+#include "ComponentTraits.h"
+#include "ComponentManager.h"
 
 namespace dom
 {
+    /**
+    * \brief A struct that holds meta information for several entities.
+    * That information can tell for each component type i what index it
+    * has in the componentList of the entity.
+    * Note that all entities with the same bitset (that is, the same
+    * components assigned) share the same entityData.
+    */
+    template<typename CINDEX = unsigned char, CINDEX COMP_TOTAL = 64>
+    class EntityData
+    {
+    friend class Entity<CINDEX, COMP_TOTAL>;
+    private:
+        std::array<CINDEX, COMP_TOTAL> mMetaData;
+        unsigned mSharedCount;
+
+    public:
+        EntityData(std::bitset< COMP_TOTAL > initialMask);
+    };
+
     /**
     * \brief Class that models a specific object in the "world". Entities are simply collections of
     * components. This class can be seen as an interface to manage the components of the
@@ -27,53 +75,101 @@ namespace dom
     public:
         /**
         * \brief Test if the entity has a specific component of type C. Returns true if and only if the
-        * component is assigned.
+        * component is assigned. O(1), just a single bit check.
         */
         template<typename C>
         bool has() const;
 
         /**
         * \brief Adds a new component of type C to the entity, if no other component of that type was added before.
-        * Returns success.
+        * Returns success. O(k), where k is the number of assigned components.
         */
         template<typename C, typename ...PARAM>
-        bool add(PARAM... param);
+        bool add(PARAM&&... param);
 
         /**
-        * \brief Gets a const reference to the requested component. Throws an "No-Component-Found"-error if
-        * has<C>() is false when this method is called.
+        * \brief Gets a const reference to the requested component. Assumes that the component exists.
+        * Throws an "No-Component-Found"-error if has<C>() is false when this method is called. O(1).
         */
         template<typename C>
         const C& get() const;
 
         /**
-        * \brief Gets a reference to the requested component. Throws an "No-Component-Found"-error if
-        * has<C>() is false when this method is called.
+        * \brief Gets a const reference to the requested component. Assumes that the component exists.
+        * Throws an "No-Component-Found"-error if has<C>() is false when this method is called. O(1).
         */
         template<typename C>
         C& modify();
 
+        /**
+        * \brief Creates a new entity wrapped in a shared ptr.
+        */
+        static std::shared_ptr< Entity<CINDEX, COMP_TOTAL> > create(ComponentManager< CINDEX, COMP_TOTAL>& componentManager);
+
     private:
-    std::bitset<COMP_TOTAL> mComponentMask;
+        Entity(ComponentManager< CINDEX, COMP_TOTAL>& componentManager) : mComponentManager(componentManager) {}
+
+    private:
+        std::bitset< COMP_TOTAL > mComponentMask; ///<bit i indicates whether component with ID i is assigned
+        EntityData<CINDEX, COMP_TOTAL>* mMetaData; ///<points to metadata that all entities with the same bitset share
+        std::vector< ComponentHandle > mComponentHandles; ///<stores indices of assigned component in their managers
+
+        ComponentManager< CINDEX, COMP_TOTAL>& mComponentManager;
+
+    public:
+        //forbit copying of an entity
+        Entity(const Entity& e) = delete;
+        Entity & operator=(const Entity&) = delete;
+
+        ~Entity();
+    };
+
+    template<typename CINDEX = unsigned char, CINDEX COMP_TOTAL = 64>
+    class Universe
+    {
+
     };
 
 
     ///IMPLEMENTATION
 
     template<typename CINDEX, CINDEX COMP_TOTAL>
+    EntityData<CINDEX, COMP_TOTAL>::EntityData(std::bitset< COMP_TOTAL > initialMask) : mSharedCount(0)
+    {
+        CINDEX bitc = 0;
+        for (CINDEX i = 0; i < COMP_TOTAL; ++i)
+        {
+            if (initialMask.test(i))
+                mMetaData[i] = bitc++;
+        }
+    }
+
+    template<typename CINDEX, CINDEX COMP_TOTAL>
     template<typename C>
     bool Entity<CINDEX, COMP_TOTAL>::has() const
     {
-        //todo
-        return false;
+        return mComponentMask.test(ComponentTraits<C, CINDEX, COMP_TOTAL>::getID());
     }
 
 
     template<typename CINDEX, CINDEX COMP_TOTAL>
     template<typename C, typename ...PARAM>
-    bool Entity<CINDEX, COMP_TOTAL>::add(PARAM... param)
+    bool Entity<CINDEX, COMP_TOTAL>::add(PARAM&&... param)
     {
-        //todo
+        static std::unordered_map< unsigned long long, std::unique_ptr<EntityData<CINDEX, COMP_TOTAL>> > sEntityData;
+        if (!has<C>()) //dont add if such a component is already assigned
+        {
+            mComponentMask.set(ComponentTraits<C, CINDEX, COMP_TOTAL>::getID()); //set the bit
+            auto hashval = mComponentMask.to_ullong(); //get the hash value for the new bitset
+            auto meta = sEntityData.emplace( hashval, std::unique_ptr<EntityData<CINDEX, COMP_TOTAL>>() ); //find metadata, may construct new
+            if (meta.second)
+                meta.first->second.reset( new EntityData<CINDEX, COMP_TOTAL>(mComponentMask) );
+            mMetaData = meta.first->second.get(); //connect to the metadata
+            mMetaData->mSharedCount++;
+            ComponentHandle c = mComponentManager.template assignComponent<C, PARAM...>( std::forward<PARAM>(param)... );
+            mComponentHandles.push_back(c);
+            return true;
+        }
         return false;
     }
 
@@ -82,8 +178,8 @@ namespace dom
     template<typename C>
     const C& Entity<CINDEX, COMP_TOTAL>::get() const
     {
-        //todo
-        return C(); //omg lol instant crash
+        auto handleIndex = mMetaData->mMetaData[ ComponentTraits<C, CINDEX, COMP_TOTAL>::getID() ];
+        return mComponentManager.template getComponent<C>( mComponentHandles[handleIndex] );
     }
 
 
@@ -91,7 +187,26 @@ namespace dom
     template<typename C>
     C& Entity<CINDEX, COMP_TOTAL>::modify()
     {
-        //todo
-        return C(); //omg lol instant crash
+        auto handleIndex = mMetaData->mMetaData[ ComponentTraits<C, CINDEX, COMP_TOTAL>::getID() ];
+        return mComponentManager.template getComponent<C>( mComponentHandles[handleIndex] );
+    }
+
+    template<typename CINDEX, CINDEX COMP_TOTAL>
+    std::shared_ptr< Entity<CINDEX, COMP_TOTAL> > Entity<CINDEX, COMP_TOTAL>::create(ComponentManager< CINDEX, COMP_TOTAL>& componentManager)
+    {
+        struct makeSharedEnabler : public Entity<CINDEX, COMP_TOTAL>
+        {
+            makeSharedEnabler(ComponentManager< CINDEX, COMP_TOTAL>& componentManager) : Entity<CINDEX, COMP_TOTAL>(componentManager){}
+        };
+        return std::make_shared<makeSharedEnabler>(componentManager);
+    }
+
+    template<typename CINDEX, CINDEX COMP_TOTAL>
+    Entity<CINDEX, COMP_TOTAL>::~Entity()
+    {
+        //todo destroy assigned components
     }
 }
+
+#endif // DOM_ENTITY_H
+
--- domTest.cpp
+++ domTest.cpp
@@ -1,7 +1,85 @@
+//#include "Entity.h"
+#include "ComponentTraits.h"
+#include "ChunkedArray.h"
 #include "Entity.h"
+#include "ComponentManager.h"
 
 #define BOOST_TEST_MODULE EntityTesting
 #include <boost/test/unit_test.hpp>
+#include <iostream>
+
+
+BOOST_AUTO_TEST_CASE( chunked_array_test )
+{
+    struct Position
+    {
+        Position(float cx, float cy) : x(cx), y(cy) {}
+
+        float x;
+        float y;
+    };
+
+    dom::ChunkedArray<Position,3> pos;
+
+    //single element test
+    dom::ElementHandle h = pos.add(0.2f,5.0f);
+    BOOST_CHECK_EQUAL(pos.get(h).x, 0.2f);
+    pos.destroy(h);
+
+    //multielement test
+    dom::ElementHandle h1 = pos.add(0.2f,5.0f);
+    dom::ElementHandle h2 = pos.add(4.4f,3.0f);
+    dom::ElementHandle h3 = pos.add(100.0f,100.0f);
+    BOOST_CHECK_EQUAL(pos.get(h1).x, 0.2f);
+    BOOST_CHECK_EQUAL(pos.get(h2).x, 4.4f);
+    BOOST_CHECK_EQUAL(pos.get(h3).x, 100.0f);
+    pos.destroy(h2);
+    dom::ElementHandle h4 = pos.add(0.0f,0.0f);
+    BOOST_CHECK_EQUAL(h2.block, h4.block);
+    BOOST_CHECK_EQUAL(h2.index, h4.index);
+    pos.destroy(h1);
+    pos.destroy(h3);
+    pos.destroy(h4);
+    h1 = pos.add(0.2f,5.0f);
+    h2 = pos.add(4.4f,3.0f);
+    h3 = pos.add(100.0f,100.0f);
+    h4 = pos.add(100.0f,100.0f);
+    BOOST_CHECK_EQUAL(pos.blockCount(), 2u);
+}
+BOOST_AUTO_TEST_CASE( component_id_test )
+{
+    struct Position
+    {
+        Position(float cx, float cy) : x(cx), y(cy) {}
+
+        float x;
+        float y;
+    };
+    struct Size
+    {
+        Size(float cwidth, float cheight) : width(cwidth), height(cheight) {}
+
+        float width;
+        float height;
+    };
+    struct TooMuch
+    {
+    };
+
+    auto i1 = dom::ComponentTraits<Position, unsigned char, 2>::getID();
+    auto i2 = dom::ComponentTraits<Size, unsigned char, 2>::getID();
+    BOOST_CHECK_EQUAL( i1, 0 );
+    BOOST_CHECK_EQUAL( i2, 1 );
+    try
+    {
+        std::cout << "Testing error message for too many components." << std::endl;
+        dom::ComponentTraits<TooMuch, unsigned char, 2>::getID();
+    }
+    catch(const dom::ComponentCountError& e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+}
 
 BOOST_AUTO_TEST_CASE( test1 )
 {
@@ -13,6 +91,9 @@ BOOST_AUTO_TEST_CASE( test1 )
         float y;
     };
 
-    dom::Entity<> e;
-    BOOST_CHECK( e.add<Position>(1.0f,10.0f) );
+    dom::ComponentManager<> cm;
+
+    std::shared_ptr<dom::Entity<>> e = dom::Entity<>::create(cm);
+    e->add<Position>(33,0);
+    BOOST_CHECK_EQUAL( e->get<Position>().x, 33 );
 }
